c83161c724f96030de0e7298e3560f9cf89866bd
Dima163163 <khlopovdima@gmail.com>
2025-08-28 21:25:14 +0400

    checkbox fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-
+'use client';
 import {Button} from "@/shared/ui/Button/Button";
 import {CustomCheckbox} from '@/shared/ui/Checkbox/Checkbox';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
+import {useState} from 'react'
 
 
 export default function Home() {
+    const [isChecked, setIsChecked] = useState(false)
   return (
     <div>
       <main>
@@ -21,9 +23,9 @@ export default function Home() {
               </Button>
           </div>
           <div style={{display: 'flex', flexDirection: 'column'}}>
-              <CustomCheckbox text={'my checkbox'} id={'ca1'}/>
+              <CustomCheckbox text={'my checkbox'} id={'ca1'} checked={isChecked} onChangeAction={setIsChecked}/>
               <CustomCheckbox id={'ca2'} />
-              <CustomCheckbox id={'ca3'} disabled={true} checked={true}/>
+              <CustomCheckbox id={'ca3'} disabled={true} checked={true} />
               <CustomCheckbox id={'ca4'} disabled={true} checked={false} text={'custom check'}/>
           </div>
       </main>
