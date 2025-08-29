6d005b645526552f7efd9b3be098c62f3aca0acb
Hrytsuki <igorgrytsuk@yandex.ru>
2025-08-29 12:30:57 +0300

    only focus style not made

--- src/shared/ui/DatePicker/SimpleDatePicker/SimpleDatePicker.tsx
+++ src/shared/ui/DatePicker/SimpleDatePicker/SimpleDatePicker.tsx
@@ -9,62 +9,55 @@ import {CalendarOutline} from "@/shared/ui/DatePicker/icons/CalendarOutline";
 import {sharedDatePickerClassNames} from "@/shared/ui/DatePicker/ClassNames";
 import {useState} from "react";
 import {CalendarOpened} from "@/shared/ui/DatePicker/icons/CalendarOpened";
+import {formatDate, isWeekend} from "@/shared/ui/DatePicker/utils/utils";
 
 
 export type DatePickerSingleProps = {
     value?: Date;
     onDateChange?: (date: Date) => void;
     label?: string;
+    error?:boolean
 } & Omit<DayPickerProps, "mode" | "selected" | "onSelect">;
 
 export const SimpleDatePicker = ({
                                      value,
                                      onDateChange,
+
                                      label = "Select Date",
                                      ...restProps
                                  }: DatePickerSingleProps) => {
     const [opened,setIsOpened]=useState(false)
+    const [error,setError]=useState(false);
+    const [disabled,setIsDisabled]=useState(true);
+    const handleOpen = () => {
+        setIsOpened(prev=>!prev);
+    }
     const handleSelect = (date: Date | undefined) => {
-        console.log(date)
+
         if (date) {
             onDateChange?.(date);
         }
     };
-
-    const formatDate = (date: Date) => {
-        if (!date) return '';
-
-        const day = date.getDate().toString().padStart(2, '0');
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const year = date.getFullYear();
-
-        return `${day}.${month}.${year}`;
-    };
-const handleOpen = () => {
-    setIsOpened(prev=>!prev);
-}
-    const isWeekend = (date:Date) => {
-        const day = date.getDay();
-        return day === 0 || day === 6; // 0 = воскресенье, 6 = суббота
-    };
     return (
         <div>
             <div className={s.text}>{label}</div>
-            <Popover.Root onOpenChange={handleOpen}>
+            <Popover.Root  onOpenChange={handleOpen}>
                 <Popover.Trigger asChild >
-                    <div className={clsx(s.datePicker)}>
-                        <div>{value ? value.toLocaleDateString() : formatDate(new Date())}</div>
+                    <div className={clsx(s.datePicker, { [s.error]: error },{ [s.disabled]: disabled })}>
+                        <div>{value ? formatDate(value) : formatDate(new Date())}</div>
                         {!opened ? <CalendarOutline/> :<CalendarOpened /> }
                     </div>
                 </Popover.Trigger>
+                {error && (
+                    <div className={s.errorMessage}>Error</div>
+                )}
                 <Popover.Portal>
                     <Popover.Content>
                         <div className={s.wrapperCalendar}>
                             <DayPicker
                                 mode="single"
-                                selected={value}
+                                selected={new Date()}
                                 onSelect={handleSelect}
-
                                 ISOWeek
                                 showOutsideDays
                                 modifiers={{ weekend: isWeekend }}
@@ -75,7 +68,6 @@ const handleOpen = () => {
                         </div>
                     </Popover.Content>
                 </Popover.Portal>
-
             </Popover.Root>
         </div>
     );
