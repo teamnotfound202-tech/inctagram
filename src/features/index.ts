0b0d72c0fe2044786ddfbdc76f5ac655a06d06fc
Dima163163 <khlopovdima@gmail.com>
2025-09-09 19:53:33 +0400

    publicUserApi

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,1 +1,2 @@
 export {authApi} from './auth/api/authApi'
+export {publicUserApi} from '@/features/publicUserApi/publicUserApi'
---
+++ src/features/publicUserApi/types.ts
@@ -0,0 +1,3 @@
+export type GetPublicUsers = {
+    "totalCount": number
+}
