08199295046bd72ad0ea9a2925b5450d72ec4837
Evghenii90 <litvinenkoevgeniy3359@gmail.com>
2025-09-15 14:22:28 +0300

    fix CreateNewPassword.tsx

--- src/views/password-recovery/ui/password-recovery/create-new-password/CreateNewPassword.tsx
+++ src/views/password-recovery/ui/password-recovery/create-new-password/CreateNewPassword.tsx
@@ -4,7 +4,7 @@ import { FormWrapper } from '@/shared/ui'
 import dynamic from 'next/dynamic'
 
 const CreateNewPasswordForm = dynamic(
-  () => import('@/features/auth/ui/passwordRecovering/ui/CreateNewPasswordForm/CreateNewPassword'),
+  () => import('@/features/auth/passwordRecovering/ui/CreateNewPasswordForm/CreateNewPassword'),
   {
     ssr: false,
     loading: () => <div>Loading...</div>,
