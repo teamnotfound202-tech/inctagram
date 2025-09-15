48a57d66879bb13f222d5a2f32e097decbe368cf
Valeria A <offtotheraces@inbox.ru>
2025-09-15 13:14:11 +0300

    build fix

--- src/views/password-recovery/ui/password-recovery/create-new-password/CreateNewPassword.tsx
+++ src/views/password-recovery/ui/password-recovery/create-new-password/CreateNewPassword.tsx
@@ -1,5 +1,15 @@
-import CreateNewPasswordForm from '@/features/auth/ui/passwordRecovering/ui/CreateNewPasswordForm/CreateNewPassword'
+'use client'
+
 import { FormWrapper } from '@/shared/ui'
+import dynamic from 'next/dynamic'
+
+const CreateNewPasswordForm = dynamic(
+  () => import('@/features/auth/ui/passwordRecovering/ui/CreateNewPasswordForm/CreateNewPassword'),
+  {
+    ssr: false,
+    loading: () => <div>Loading...</div>,
+  }
+)
 
 export default function CreateNewPassword() {
   return (
