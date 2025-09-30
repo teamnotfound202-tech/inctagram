27f4d3beda256fdc9d972b745b8ce58e49700c72
1rko <irkoabc@gmail.com>
2025-09-30 22:37:40 +0300

    feat: add embla-carousel, slider

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
 export default function Page() {
-  return <div>Private page</div>
+    return <div>
+        Private page
+    </div>
 }
--- src/features/postView/ui/PostSSR/PostView/PostImage/PostImage.tsx
+++ src/features/postView/ui/PostSSR/PostView/PostImage/PostImage.tsx
@@ -1,18 +1,31 @@
 import s from "./PostImage.module.scss";
-import {Carousel} from "@/features/postView/ui/PostSSR/PostView/PostImage/Сarousel/Сarousel";
 import {ImagePost} from "@/features/postView/api/types";
-import ImageCarousel
-    from "@/features/postView/ui/PostSSR/PostView/PostImage/ImageCarouselWithSwiper/ImageCarouselWithSwiper";
+import EmblaCarousel from "@/features/postView/ui/PostSSR/PostView/PostImage/EmblaCarousel/EmblaCarousel";
+import {EmblaOptionsType} from "embla-carousel";
 
 type Props = {
     imageUrl?: string;
     images:ImagePost[]
 }
 
+
+
 export const PostImage = ({images}: Props) => {
+     let imagesUrl = images.map(postImage=>postImage.url)
+    imagesUrl=[...imagesUrl]
+
+    const carouselOptions: EmblaOptionsType = {
+        loop: true,
+        align: 'center',
+        skipSnaps: false,
+        duration: 20, // скорость анимации
+    };
     return (
         <div className={s.postImageWrapper}>
-            <ImageCarousel imagesArray={images}/>
+            <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+                <EmblaCarousel slides={imagesUrl} options={carouselOptions} />
+            </div>
+            {/*<ImageCarousel imagesArray={images}/>TODO: удалить лишние карусели и библиотеки*/}
             {/*<Carousel imagesArray={images}/>*/}
            {/* <img src={imageUrl} alt={'postImage'} className={s.postImage}/>*/}
         </div>
