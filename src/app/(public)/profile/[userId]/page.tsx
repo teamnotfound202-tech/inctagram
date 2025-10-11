956b2a7a59d48dc697e573651b82b5784b34d86a
Hrytsuki <igorgrytsuk@yandex.ru>
2025-10-11 13:41:31 +0300

    error as string added

--- src/app/(public)/profile/[userId]/page.tsx
+++ src/app/(public)/profile/[userId]/page.tsx
@@ -8,7 +8,7 @@ export default async function ProfilePage (props: {
   params: Promise<{ userId: string }>,
 })  {
   const params = await props.params;
-  const resp = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/posts/user/${params.userId}/,?pageSize=${PAGINATION.DEFAULT_PAGE_SIZE}`)
+  const resp = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/posts/user/${params.userId}/?pageSize=${PAGINATION.DEFAULT_PAGE_SIZE}`)
   const postsData = await resp.json()
 
   const userData = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/public-user/profile/${params.userId}`)
