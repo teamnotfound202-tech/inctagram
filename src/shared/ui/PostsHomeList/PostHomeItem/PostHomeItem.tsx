e471b8288c47281dabccefbaa5578604e50f2f00
Alexandr_Lukomskiy <Lukomskiy.AC@gmail.com>
2025-10-16 14:16:49 +0200

    Home page

---
+++ src/shared/ui/PostsHomeList/PostHomeItem/PostHomeItem.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import s from './PostHomeItem.module.scss'
+import {Post} from '@/features/publicUserApi/types'
+import 'swiper/css';
+import {useEffect, useState} from 'react'
+import Link from 'next/link'
+import {usePathname, useSearchParams} from 'next/navigation'
+import {LinkContent} from "@/views/ProfilePosts/PostItem/LinkContent/LinkContent";
+import Avatar from '../../../../entities/user/ui/Avatar/Avatar'
+import { getTimeDifference } from '@/shared/lib/utils/getTimeDifference'
+
+type Props = {
+  post: Post
+}
+
+export const PostHomeItem = ({post}: Props) => {
+  const [path, setPath] = useState('');
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const currentSearchParams = new URLSearchParams(searchParams.toString());
+    currentSearchParams.set('postId', post.id.toString());
+    setPath(`${pathname}?${currentSearchParams.toString()}`);
+    console.log(currentSearchParams)
+  }, [pathname, searchParams, post.id]);
+
+  return (
+    <li className={s.postItem}>
+      <Link href={path} prefetch={false}>
+        {/* Передаем управление дочернему компоненту */}
+        <LinkContent post={post} />
+      </Link>
+
+      <div className={s.userInfo}>
+        <Avatar src={post.avatarOwner} alt="Avatar Image" size="small" />
+        <Link className={s.userName} href={`/profile/${post.ownerId}`} prefetch={true}>
+          <span className={s.userName}>{post.userName}</span>
+        </Link>
+      </div>
+
+      <span className={s.time}>{getTimeDifference(post.createdAt)}</span>
+
+      <p className={s.description}>
+        {post.description}
+
+      </p>
+    </li>
+  );
+}
