9e4c6e8f247091257c0684a87e7a2c8840b7ea67
1rko <irkoabc@gmail.com>
2025-10-17 18:43:25 +0300

    fix: delete unused variable

--- src/app/(public)/profile/[userId]/@modal/(.)post/[postId]/page.tsx
+++ src/app/(public)/profile/[userId]/@modal/(.)post/[postId]/page.tsx
@@ -1,13 +1,10 @@
 import {PostSsr} from "@/features/postView";
 
-export default async function ModalLayer({params, searchParams}: {
-    params: Promise<{ userId: string, postId:string }>,
-    searchParams: Promise<{ postId?: string }>
+export default async function ModalLayer({params}: {
+    params: Promise<{ userId: string, postId:string }>
 }) {
-    const awaitedSearchParams = await searchParams;
     const awaitedParams = await params;
-    //if (!awaitedSearchParams?.postId) return null
-    return <PostSsr params={awaitedParams} searchParams={awaitedSearchParams}/>
+    return <PostSsr params={awaitedParams} />
 }
 
 
