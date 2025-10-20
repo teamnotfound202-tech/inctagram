327bb618c51ad885866cec8ff5504689bb675767
Dima163163 <khlopovdima@gmail.com>
2025-10-20 16:39:16 +0400

    fix PostSSr

--- src/app/(public)/profile/[userId]/@modal/(.)post/[postId]/page.tsx
+++ src/app/(public)/profile/[userId]/@modal/(.)post/[postId]/page.tsx
@@ -3,8 +3,7 @@ import {PostSsr} from "@/features/postView";
 export default async function ModalLayer({params}: {
     params: Promise<{ userId: string, postId:string }>
 }) {
-    const awaitedParams = await params;
-    return <PostSsr params={awaitedParams} />
+    return <PostSsr params={params} />
 }
 
 
