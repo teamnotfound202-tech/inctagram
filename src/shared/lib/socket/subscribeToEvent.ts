25b051a2de4342c3ca7152fbca8267bdd2f6ca85
Hrytsuki <igorgrytsuk@yandex.ru>
2025-12-09 18:07:52 +0300

    Revert "new messengerfor me"
    This reverts commit ce5c9b95
    
    # Conflicts:
    #	src/shared/api/baseApi.ts
    #	src/shared/lib/constants/constants.ts
    #	src/widgets/Sidebar/Sidebar.tsx

--- src/shared/lib/socket/subscribeToEvent.ts
+++ src/shared/lib/socket/subscribeToEvent.ts
@@ -1,11 +1,28 @@
-import {getSocket} from '@/shared/lib/socket/getSocket'
-import {SocketEvent} from '@/shared/lib/constants/constants'
+import { getSocket } from '@/shared/lib/socket/getSocket'
+import { SocketEvent } from '@/shared/lib/constants/constants'
 
-export const subscribeToEvent = <T>(event: SocketEvent, callback: (data: T) => void) => {
-    const socket = getSocket()
-    socket.on(event, callback)
-    return () => {
-        socket.off(event, callback)
-    }
+export const subscribeToEvent = <T>(event: SocketEvent, callback:(data:T)=>void) => {
+  const socket = getSocket()
+  socket.on(event,callback)
+  return () => {
+    socket.off(event,callback)
+  }
 }
+export const emitEvent = <T, R = any>(
+  event: string,
+  data: T,
+  callback?: (response: R) => void
+): void => {
+  const socket = getSocket() // предполагаем синхронный вызов
 
+  if (!socket || !socket.connected) {
+    console.error(`Cannot emit ${event}: socket not connected`)
+    return
+  }
+
+  if (callback) {
+    socket.emit(event, data, callback) // ⚡️ Socket.IO поддерживает колбэки!
+  } else {
+    socket.emit(event, data)
+  }
+}
